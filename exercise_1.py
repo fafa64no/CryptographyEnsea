1b94b8aa91fb2a56a6c07805bc800ab306bbb3ed
Sébastien Nouira Huet <148809822+fafa64no@users.noreply.github.com>
2025-12-01 14:14:14 +0100

    [ADD] Q1.3

--- exercise_1.py
+++ exercise_1.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from Crypto.Hash.MD5 import MD5Hash
 from Crypto.Hash.SHA1 import SHA1Hash
 
@@ -9,7 +11,8 @@ DEFAULT_STR_LIST = [
 ]
 
 DEFAULT_BIG_STR_LIST = [
-    open("data/BigText.txt").read().encode("utf-8")
+    open("data/BigText.txt").read(),
+    open("data/BigText.modified.txt").read(),
 ]
 
 def test_md5_hash() -> None:
@@ -27,6 +30,8 @@ def test_sha_1_hash() -> None:
 def test_long_hash() -> None:
     print("============================== MD5 Long ==============================")
     for s in DEFAULT_BIG_STR_LIST:
+        s = s.encode("utf-8")
+
         test_hash_md5 = MD5Hash(s)
         test_hash_sha_1 = SHA1Hash(s)
         print(f"Text as MD5 hash is {test_hash_md5.hexdigest()}")
